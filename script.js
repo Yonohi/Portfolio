58b748923930bc82d77f69c33bb57d2c4b61e2d5
Yonohi <loic.planat2@gmail.com>
2022-05-23 23:33:41 +0200

    hover projets

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Création des listes et autres variables
 let projects = [];
 let cards = [];
-let imgProjects = [];
 let modal = document.getElementsByClassName('modal')[0];
 let imgModal = document.getElementsByClassName('img_modal')[0];
 let body = document.getElementsByTagName('body')[0];
@@ -11,7 +10,6 @@ for (let i = 1; i <= 13; i++) {
     projects.push(document.getElementById(`project_${i}`));
     cards.push(document.getElementById(`card_${i}`));
 };
-imgProjects.push(document.getElementsByClassName('proj_img'));
 
 // Cette boucle est obligatoire si l'on veut que le premier click soit pris en compte
 for (let i = 1; i <= 13; i++) {
@@ -23,18 +21,15 @@ for (let i = 1; i <= 13; i++) {
         for (let j = 1; j <= 13; j++) {
             if ((i%2 == 0 && j == (i-1)) || (i%2 == 1 && j == (i+1))){
                 cards[j-1].style.display = 'none';
-                projects[j-1].style.borderColor = "rgb(247, 233, 235)";
-                projects[j-1].style.backgroundColor = "rgb(247, 233, 235)";
+                projects[i-1].classList.remove('active');
             } 
         };
         if (cards[i-1].style.display == 'none') {
-            projects[i-1].style.borderColor = "black";
-            projects[i-1].style.backgroundColor = '#ecacac';
+            projects[i-1].classList.add('active');
             cards[i-1].style.display = 'flex';
         } else {
             cards[i-1].style.display = 'none';
-            projects[i-1].style.borderColor = "rgb(247, 233, 235)";
-            projects[i-1].style.backgroundColor = "rgb(247, 233, 235)";
+            projects[i-1].classList.remove('active');
         };
     })
 };
@@ -49,11 +44,3 @@ function closeModal() {
     modal.style.display = 'none';
     body.style.overflow = "scroll";
 };
-// function biggerImg(img) {
-//     img.classList.add('active');
-// };
-// for (let imgProject of imgProjects) {
-//     imgProject.addEventListener('click', function() {
-//         imgProject.style.transform = 'scale(2)';
-//     });
-// };
