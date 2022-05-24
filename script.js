8a93aa9cae92cee537a17ac9780e7bb05175e53c
Yonohi <loic.planat2@gmail.com>
2022-05-24 12:53:15 +0200

    changement cv charte graphique

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Création des listes et autres variables
+let nbProject = 13;
 let projects = [];
 let cards = [];
 let modal = document.getElementsByClassName('modal')[0];
@@ -6,19 +7,19 @@ let imgModal = document.getElementsByClassName('img_modal')[0];
 let body = document.getElementsByTagName('body')[0];
 
 // Récupération des élément du DOM
-for (let i = 1; i <= 13; i++) {
+for (let i = 1; i <= nbProject; i++) {
     projects.push(document.getElementById(`project_${i}`));
     cards.push(document.getElementById(`card_${i}`));
 };
 
 // Cette boucle est obligatoire si l'on veut que le premier click soit pris en compte
-for (let i = 1; i <= 13; i++) {
+for (let i = 1; i <= nbProject; i++) {
     cards[i-1].style.display = 'none'
 };
 
-for (let i = 1; i <= 13; i++) {
+for (let i = 1; i <= nbProject; i++) {
     projects[i-1].addEventListener('click', function() {
-        for (let j = 1; j <= 13; j++) {
+        for (let j = 1; j <= nbProject; j++) {
             if ((i%2 == 0 && j == (i-1)) || (i%2 == 1 && j == (i+1))){
                 cards[j-1].style.display = 'none';
                 projects[j-1].classList.remove('active');
